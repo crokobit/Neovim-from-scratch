4148c337e9f18826984f1b3a15178056c19c3b2d
crokobit <crokobit@gmail.com>
2022-04-15 21:05:02 +0800

    t

--- bash.sh
+++ bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker build --no-cache . -t mynvim
+#docker build --no-cache . -t mynvim
+docker build . -t mynvim
 #docker run -it --rm mynvim -d
-docker run -d mynvim tail -f /dev/null
+#docker run -d mynvim tail -f /dev/null
