2ebabaa54b35c0c33b0c001344f0fe323e5908dd
crokobit <crokobit@gmail.com>
2023-12-28 23:32:07 +0800

    T

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,1 +1,20 @@
-
+require("copilot").setup({
+  panel = {
+    auto_refresh = true,
+    keymap = {
+      accept = "<CR>",
+      jump_prev = "[[",
+      jump_next = "]]",
+      refresh = "gr",
+      open = "<M-CR>",
+    },
+  },
+  suggestion = {
+    auto_trigger = true,
+    keymap = {
+      accept = "<M-l>",
+      prev = "<M-[>",
+      next = "<M-]>",
+      dismiss = "<C-]>",
+    },
+  },
